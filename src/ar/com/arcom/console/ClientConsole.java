b2b4e580e92060c249999afc7d82eb6bce079f16
FGMC125 <fgmc125@gmail.com>
2022-07-05 07:22:25 -0300

    Fix

--- src/ar/com/arcom/console/ClientConsole.java
+++ src/ar/com/arcom/console/ClientConsole.java
@@ -4,12 +4,14 @@ import ar.com.arcom.Application;
 import ar.com.arcom.bin.Articulo;
 import ar.com.arcom.bin.Cliente;
 import ar.com.arcom.handlers.ActionHandler;
+import ar.com.arcom.handlers.UIHelper;
 
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class ClientConsole {
+public class ClientConsole implements UIHelper {
     private Application application;
     private ActionHandler actionHandler;
 
@@ -20,56 +22,53 @@ public class ClientConsole {
 
     // ----------------------------------------------------------------
     // Métodos de flujo
+    // ----------------------------------------------------------------
     private int scanner(){
         System.out.print("Respuesta: ");
         Scanner scanner = new Scanner(System.in);
         return scanner.nextInt();
     }
-    public void menuCliente(){
+    public void menuClienteFlujo(){
         int respuesta;
         do {
             respuesta = menuPrincipalCliente();
             switch (respuesta){
-                case 0 -> {
-                    actionHandler.cerrarSesion();
-                }
-                case 1 -> {menuVerProductoCliente();}
-                case 2 -> {menuCarritoCliente();}
+                case 0 -> actionHandler.cerrarSesion();
+                case 1 -> actionHandler.configuraUI(this,2);
+                case 2 -> actionHandler.configuraUI(this,3);
                 default -> {}
             }
         }while (respuesta != 0);
     }
-    public void menuVerProductoCliente(){
+    public void menuVerProductoFlujo(){
         int respuesta;
         do {
-            respuesta = menuProductosCliente();
+            respuesta = menuVerProductos();
             switch (respuesta){
                 case 1 -> {
-                    int id = ingreseID();
+                    int id = ingreseID("agregar");
                     if(id != 0){
-                        int cantidad = ingreseCantidadAgregar(id);
+                        int cantidad = ingreseCantidad(id, "agregar");
                         if (cantidad != 0) {
                             actionHandler.agregaCarrito(id,cantidad);
                             ((Cliente)(application.getUsuario())).agregar(new Articulo(id,cantidad));
                         }
                     }
                 }
-                case 2 -> {menuVerCarritoCliente();}
+                case 2 -> actionHandler.configuraUI(this,3);
+                case 3 -> {}
                 default -> {}
             }
         }while (respuesta != 0);
     }
-    public void menuVerCarritoCliente(){
+    public void menuVerCarritoFlujo(){
         int respuesta;
         do {
-            respuesta = menuCarritoCliente();
+            respuesta = menuVerCarrito();
             switch (respuesta){
-                case 1 -> {
-                    menuVerProductoCliente();
-                }
-                case 2 -> {
-                    menuModificarProductoCarritoFlujo();
-                }
+                case 1 -> {menuVerProductoFlujo();}
+                case 2 -> {menuModificarProductoCarritoFlujo();}
+                case 3 -> {menuCompraFlujo();}
                 default -> {}
             }
         }while (respuesta != 0);
@@ -80,9 +79,9 @@ public class ClientConsole {
             respuesta = menuModificarProductoDelCarrito();
             switch (respuesta){
                 case 1 -> {
-                    int id = ingreseID();
+                    int id = ingreseID("agregar");
                     if(id != 0){
-                        int cantidad = ingreseCantidadAgregar(id);
+                        int cantidad = ingreseCantidad(id, "agregar");
                         if (cantidad != 0) {
                             actionHandler.modificarArticuloCarrito(id,cantidad);
                             ((Cliente)(application.getUsuario())).getCarrito().agregaCantidad(id,cantidad);
@@ -90,9 +89,9 @@ public class ClientConsole {
                     }
                 }
                 case 2 -> {
-                    int id = ingreseID();
+                    int id = ingreseID("quitar");
                     if(id != 0){
-                        int cantidad = ingreseCantidadQuitar(id);
+                        int cantidad = ingreseCantidad(id, "quitar");
                         if (cantidad != 0) {
                             ((Cliente)(application.getUsuario())).getCarrito().quitarCantidad(id,cantidad);
                             actionHandler.modificarQuitarArticuloCarrito(id,cantidad);
@@ -103,9 +102,12 @@ public class ClientConsole {
             }
         }while (respuesta != 0);
     }
+    private void menuCompraFlujo() {
+    }
 
     // ----------------------------------------------------------------
     // Métodos de opciones
+    // ----------------------------------------------------------------
     public int menuPrincipalCliente() {
         System.out.println("----------------------------------------------------");
         System.out.println(application.TITULO + " : Menu Principal");
@@ -113,14 +115,42 @@ public class ClientConsole {
                 + application.getUsuario().getNombre().substring(1));
         System.out.println("----------------------------------------------------");
         System.out.println("Ingrese [valor] correspondiente a la opcion elegida.");
-        System.out.println("[1] Ver productos");
-        System.out.println("[2] Ver carrito");
+        System.out.println("[1] Ver Productos.");
+        System.out.println("[2] Ver Carrito.");
         System.out.println("");
-        System.out.println("[0] Cerrar Sesion");
+        System.out.println("[0] Cerrar Sesion.");
         System.out.println("----------------------------------------------------");
         return scanner();
     }
-    public int menuCarritoCliente() {
+    public int menuVerProductos(){
+        System.out.println("----------------------------------------------------");
+        System.out.println(application.TITULO + " : Menu Ver Productos");
+        System.out.println("Usuario: " + application.getUsuario().getNombre().toUpperCase().charAt(0)
+                + application.getUsuario().getNombre().substring(1));
+        System.out.println("----------------------------------------------------");
+
+        List<List<String>> lista = actionHandler.obtenerProductos();
+
+        System.out.println(" ID | Producto | Precio | Existencias |");
+
+        for (List<String> cadena : lista) {
+            for (String str : cadena) {
+                System.out.print(str); System.out.print(" | ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("----------------------------------------------------");
+        System.out.println("Ingrese [valor] correspondiente a la opcion elegida.");
+        System.out.println("[1] Seleccionar Producto.");
+        System.out.println("[2] Buscar Producto.");
+        System.out.println("[3] Ver Carrito.");
+        System.out.println("");
+        System.out.println("[0] Volver.");
+        System.out.println("----------------------------------------------------");
+        return scanner();
+    }
+    public int menuVerCarrito() {
         System.out.println("----------------------------------------------------");
         System.out.println(application.TITULO + " : Menu Ver Carrito");
         System.out.println("Usuario: " + application.getUsuario().getNombre().toUpperCase().charAt(0)
@@ -141,15 +171,14 @@ public class ClientConsole {
 
         System.out.println("----------------------------------------------------");
         System.out.println("Ingrese [valor] correspondiente a la opcion elegida.");
-        System.out.println("[1] Agregar un Producto");
-        System.out.println("[2] Modificar");
-        System.out.println("[3] Comprar");
+        System.out.println("[1] Agregar un Producto.");
+        System.out.println("[2] Modificar.");
+        System.out.println("[3] Comprar.");
         System.out.println("");
-        System.out.println("[0] Volver");
+        System.out.println("[0] Volver.");
         System.out.println("----------------------------------------------------");
         return scanner();
     }
-
     public int menuModificarProductoDelCarrito(){
         System.out.println("----------------------------------------------------");
         System.out.println(application.TITULO + " : Modificar Producto del Carrito");
@@ -177,91 +206,67 @@ public class ClientConsole {
         return scanner();
     }
 
-
-    public int menuProductosCliente(){
-        System.out.println("----------------------------------------------------");
-        System.out.println(application.TITULO + " : Menu Ver Productos");
-        System.out.println("Usuario: " + application.getUsuario().getNombre().toUpperCase().charAt(0)
-                + application.getUsuario().getNombre().substring(1));
-        System.out.println("----------------------------------------------------");
-
-        List<List<String>> lista = actionHandler.obtenerProductos();
-
-        /*int[] mayor = {0,0,0,0,0};
-        int aux=0;
-        for (int i = 0; i < lista.size(); i++) {
-            for (int j = 0; j < lista.get(i).size(); j++) {
-                aux = lista.get(i).get(j).length();
-                if (mayor[i]<aux) mayor[i]=aux;
-            }
-        }
-        */
-
-        System.out.println(" ID | Producto | Precio | Cantidad | Total |");
-
-        for (List<String> cadena : lista) {
-            for (String str : cadena) {
-                System.out.print(str); System.out.print(" | ");
-            }
-            System.out.println();
-        }
-        System.out.println("----------------------------------------------------");
-        System.out.println("Ingrese [valor] correspondiente a la opcion elegida.");
-        System.out.println("[1] Seleccionar Producto");
-        System.out.println("[2] Ver Carrito");
-        System.out.println("[3] Buscar Producto");
-        System.out.println("");
-        System.out.println("[0] Volver");
-        System.out.println("----------------------------------------------------");
-        return scanner();
-    }
-
-    private int ingreseID(){
+    private int ingreseID(String modo){
         boolean aux = false;
         int id;
         do{
             System.out.println("----------------------------------------------------");
-            System.out.println("Ingrese el ID de producto o ingrese 0 para cancelar.");
+            System.out.println("Ingrese el ID del producto a " + modo + " o ingrese 0 para cancelar.");
             id = scanner();
-            if(verificaID(id)) aux = true;
+            if(verificaID(id, modo)) aux = true;
+            if (!aux) {
+                System.out.println("----------------------------------------------------");
+                System.out.println("-------->El valor ingresado es incorrecto <---------");
+            }
         }while (!aux && id != 0 );
         return id;
     }
-
-    private int ingreseCantidadAgregar(int id){
+    private int ingreseCantidad(int id, String cadena){
         boolean aux = false;
         int cantidad;
         do{
             System.out.println("----------------------------------------------------");
-            System.out.println("Ingrese la cantidad o ingrese 0 para cancelar.");
+            System.out.println("Ingrese la cantidad a " + cadena + " o ingrese 0 para cancelar.");
             cantidad = scanner();
-            if(cantidad !=0 && verificaCantidadAgregar(id, cantidad)) aux = true;
-        }while (!aux && cantidad != 0);
-        return cantidad;
-    }
-    private int ingreseCantidadQuitar(int id) {
-        boolean aux = false;
-        int cantidad;
-        do{
-            System.out.println("----------------------------------------------------");
-            System.out.println("Ingrese la cantidad o ingrese 0 para cancelar.");
-            cantidad = scanner();
-            if(cantidad !=0 && verificaCantidadQuitar(id, cantidad)) aux = true;
+            if(cantidad !=0 && verificaCantidad(id, cantidad, cadena.equalsIgnoreCase("agregar")))
+                aux = true;
+            if (!aux) {
+                System.out.println("----------------------------------------------------");
+                System.out.println("-------->El valor ingresado es incorrecto <---------");
+            }
         }while (!aux && cantidad != 0);
         return cantidad;
     }
 
-    private boolean verificaCantidadQuitar(int id, int cantidad) {
-        return ((Cliente)(application.getUsuario())).getCarrito().verificaCantidad(id,cantidad);
+    private boolean verificaCantidad(int id, int cantidad, boolean operacion) {
+        if (operacion) return actionHandler.consultaStock(id, cantidad);
+        else return ((Cliente)(application.getUsuario())).getCarrito().verificaCantidad(id,cantidad);
     }
-
-    private boolean verificaCantidadAgregar(int id, int cantidad) {
-        return actionHandler.consultaStock(id, cantidad);
-    }
-    private boolean verificaID(int valor) {
-        return true;
+    private boolean verificaID(int id, String modo) {
+        if (modo.equalsIgnoreCase("agregar")) return actionHandler.consultaSiExiste(id);
+        else return ((Cliente)application.getUsuario()).getCarrito().consultaSiExisteID(id);
     }
 
     // ----------------------------------------------------------------
     // Métodos de implementacion
+    // ----------------------------------------------------------------
+    @Override
+    public void configuraUI(int valor) {
+        switch (valor){
+            case 1 -> menuClienteFlujo();
+            case 2 -> menuVerProductoFlujo();
+            case 3 -> menuVerCarritoFlujo();
+            case 4 -> JOptionPane.showMessageDialog(null, "Usuario ya existe");
+            case 5 -> JOptionPane.showMessageDialog(null, "Error al conectar al servidor, intente de nuevo");
+            default -> {}
+        }
+    }
+    @Override
+    public void cargaLista(int valor) {
+
+    }
+    @Override
+    public void ultimosArreglos(int valor) {
+
+    }
 }
